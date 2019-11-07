7f06d0d809e3d41cc36777b66c976a0517bbf6cd
huhao <canghailan@gmail.com>
2019-11-07 11:22:54 +0800

    添加kubectl配置管理工具

---
+++ kubernetes/kube-cfg.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import shutil
+from pathlib import Path
+
+
+def use(args):
+    kube = Path.home() / ".kube"
+    src = kube / "config.d" / args.name
+    dst = kube / "config"
+    shutil.copyfile(src, dst)
+    print(f"use {src}")
+
+
+def save(args):
+    kube = Path.home() / ".kube"
+    src = kube / "config"
+    dst = kube / "config.d" / args.name
+    shutil.copyfile(src, dst)
+    print(f"save {dst}")
+
+
+def delete(args):
+    kube = Path.home() / ".kube"
+    f = kube / "config.d" / args.name
+    os.remove(f)
+    print(f"delete {f}")
+
+
+parser = argparse.ArgumentParser()
+parser_subparsers = parser.add_subparsers()
+subparser_use = parser_subparsers.add_parser('use')
+subparser_use.add_argument("name")
+subparser_use.set_defaults(func=use)
+subparser_save = parser_subparsers.add_parser('save')
+subparser_save.add_argument("name")
+subparser_save.set_defaults(func=save)
+subparser_delete = parser_subparsers.add_parser('delete')
+subparser_delete.add_argument("name")
+subparser_delete.set_defaults(func=delete)
+
+args = parser.parse_args()
+args.func(args)
